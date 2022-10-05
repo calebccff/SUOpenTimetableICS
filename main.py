5a840303795b3ed7481eeaf55af4b6ec79d49f81
Caleb Connolly <caleb@connolly.tech>
2022-10-05 03:43:38 +0100

    it works?

---
+++ main.py
@@ -0,0 +1,103 @@
+from flask_cors import CORS
+from flask import Flask, request, abort
+import requests, re, csv
+from ics import Calendar
+from urllib.parse import urlparse, quote
+from time import sleep
+from opentimetable import *
+
+app = Flask(__name__)
+CORS(app)
+
+DOMAIN = "calebs.dev"
+headers = {'User-Agent': 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
+'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+'Accept-Encoding': 'gzip, deflate, br',
+'Accept-Language': 'en-US,en;q=0.9'}
+
+@app.route("/", methods=['GET'])
+def index():
+    html = """
+    <!DOCTYPE html>
+<html>
+<body>
+
+<h2>MyTimetable to Open Timetable converter</h2>
+<p>This form takes a link to a My Timetable iCal file which you can get by going to https://mytimetable.swansea.ac.uk<br>
+and returns an equivalent link you can use which will fetch the data from Open Timetable instead, as apparently<br>
+that's what we /have/ to use. The MyTimetable link is only used to figure out what modules you take, you should check<br>
+that all of your modules are in the returned URL and add them manually if not - or if they don't match the format<br>
+Open Timetable uses for them.</p>
+
+<form action="/api/generate">
+  <label for="ical">MyTimetable iCal link</label><br>
+  <input type="text" id="ical" name="ical" value=""><br>
+  <input type="submit" value="Submit">
+</form> 
+
+</body>
+</html>
+
+"""
+
+    return html
+
+# https://scientia-eu-v3-4-api-d1-04.azurewebsites.net/api/ical/c0fafdf7-2aab-419e-a69b-bbb9e957303c/141c474f-bd21-8c56-60fe-dbf755ebe241/timetable.ics
+# mytimetable_ics should be everything after /api/ical/, e.g.
+# c0fafdf7-2aab-419e-a69b-bbb9e957303c/141c474f-bd21-8c56-60fe-dbf755ebe241/timetable.ics
+@app.route('/api/generate', methods=['GET'])
+def generate():
+    mytimetable_link = request.args.get('ical')
+    print("Got link: " + mytimetable_link)
+    url = ""
+    try:
+        url = f"https://scientia-eu-v3-4-api-d1-04.azurewebsites.net{urlparse(mytimetable_link).path}"
+    except Exception as e:
+        print("Failed to parse URL!")
+        return f"Invalid URL: \"{mytimetable_link}\""
+    print(f"Requesting timetable: {url}")
+
+    resp = 0
+    for i in range(5):
+        timetable = requests.get(url, headers=headers)
+        if timetable.status_code == 200:
+            break
+        print(timetable.text)
+        sleep(0.5)
+
+    if timetable.status_code != 200:
+        return f"Failed to get timetable: {timetable.status_code}"
+
+    print(timetable.text)
+    c = Calendar(timetable.text)
+
+    print(request.form)
+
+    modules = []
+    for ev in c.events:
+        m = ev.name.split("/")[0].upper()
+        if m not in modules:
+            modules.append(m)
+
+    data = quote(';'.join(modules))
+
+    return(f"{request.url_root}api/ics?modules={data}")
+
+@app.route('/api/ics', methods=['GET'])
+def ics():
+    modules = request.args.get("modules")
+
+    return get_ical_for_modules(modules.split(";")).serialize()
+
+
+# Disable caching
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8001)
---
+++ opentimetable.py
@@ -0,0 +1,227 @@
+import requests
+import json
+from ics import Calendar, Event
+from urllib.parse import quote
+from datetime import datetime, timedelta
+from arrow import Arrow
+
+# Reference: https://github.com/Frazl/dcu-opentimetable/blob/2e1b8a8966e7573118587e6d04a3a3dbfe98eae4/timetable.py
+
+HEADERS = {
+    "Authorization": "basic kR1n1RXYhF",
+    "Content-Type" : "application/json",
+    "Referer" : "https://opentimetables.swan.ac.uk/",
+    "Origin" : "https://opentimetables.swan.ac.uk/",
+    "Host": "opentimetables.swan.ac.uk",
+    "User-Agent": "Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
+}
+
+BASE_URL = "https://opentimetables.swan.ac.uk/broker/api/CategoryTypes/525fe79b-73c3-4b5c-8186-83c652b3adcc/"
+
+def get_view_options(weeks=20):
+    view_options = {
+        "Days": [
+            {
+                "Name": "Monday",
+                "DayOfWeek": 1,
+                "IsDefault": True
+            },
+            {
+                "Name": "Tuesday",
+                "DayOfWeek": 2,
+                "IsDefault": True
+            },
+            {
+                "Name": "Wednesday",
+                "DayOfWeek": 3,
+                "IsDefault": True
+            },
+            {
+                "Name": "Thursday",
+                "DayOfWeek": 4,
+                "IsDefault": True
+            },
+            {
+                "Name": "Friday",
+                "DayOfWeek": 5,
+                "IsDefault": True
+            }
+        ],
+        "Weeks": [],
+        "TimePeriods": [
+            {
+                "Description": "Teaching Day",
+                "StartTime": "09:00",
+                "EndTime": "18:00",
+                "IsDefault": True
+            }
+        ],
+        "DatePeriods": [
+            {
+                "Description": "This Week",
+                "StartDateTime": "2022-09-26T00:00:00.000Z",
+                "EndDateTime": "2023-09-25T00:00:00.000Z",
+                "IsDefault": True,
+                "IsThisWeek": True,
+                "IsNextWeek": False,
+                "Type": "ThisWeek"
+            }
+        ],
+        "LegendItems": [],
+        "InstitutionConfig": {},
+        "DateConfig": {
+            "FirstDayInWeek": 1,
+            "StartDate": "2022-09-26T00:00:00+00:00",
+            "EndDate": "2023-09-29T00:00:00+00:00"
+        },
+        "AllDays": [
+            {
+                "Name": "Monday",
+                "DayOfWeek": 1,
+                "IsDefault": True
+            },
+            {
+                "Name": "Tuesday",
+                "DayOfWeek": 2,
+                "IsDefault": True
+            },
+            {
+                "Name": "Wednesday",
+                "DayOfWeek": 3,
+                "IsDefault": True
+            },
+            {
+                "Name": "Thursday",
+                "DayOfWeek": 4,
+                "IsDefault": True
+            },
+            {
+                "Name": "Friday",
+                "DayOfWeek": 5,
+                "IsDefault": True
+            },
+            {
+                "Name": "",
+                "DayOfWeek": 6,
+                "IsDefault": False
+            },
+            {
+                "Name": "",
+                "DayOfWeek": 0,
+                "IsDefault": False
+            }
+        ]
+    }
+
+    week1_date = datetime(2022, 9, 26)
+    until = datetime(2023, 6, 6)
+    #this_week = datetime.now() - timedelta(datetime.now().weekday())
+    view_weeks = []
+    for i in range(weeks):
+        this_week = datetime.now() - timedelta(datetime.now().weekday())
+        this_week += timedelta(days = 7 * i)
+        week_number = (this_week - week1_date).days / 7
+        view_options["Weeks"].append({
+                "WeekNumber": int(week_number),
+                "WeekLabel": "Week 2",
+                "FirstDayInWeek": this_week.isoformat() + "Z"
+            })
+    
+
+    return view_options
+
+def parse_date(date_str):
+    '''
+    Parses the date to a datetime date object
+    '''
+    year = int(date_str[:4])
+    month = int(date_str[5:7])
+    day = int(date_str[8:10])
+    return datetime.datetime(year, month, day).date()
+
+# FIXME: caching
+# returns a list of module dictionary objects with the name, Identity and CategoryTypeIdentity
+# or an error
+def get_all_modules():
+    modules = []
+    for i in range(10):
+        resp = requests.post(BASE_URL + f"Categories/Filter?pageNumber={i}", json=[], headers=HEADERS)
+        if resp.status_code != 200:
+            print(f"Failed to get modules: {resp.status_code}")
+            print(resp.text)
+            return resp.status_code
+        resp = json.loads(resp.text)
+
+        for m in resp["Results"]:
+            modules.append({"name": m["Name"], "id": m["Identity"], "category": m["CategoryTypeIdentity"]})
+
+        print(f"\n\nPage: {i}, total pages: {resp['TotalPages']}\n\n")
+        if i > resp["TotalPages"]:
+            break
+    
+    return modules
+
+def module_search(module):
+    mods = module.upper().split("_")
+    query = mods[0]
+    if mods[-1] in ["A", "B"]:
+        query += f"_{mods[-1]}"
+    print(f"Search for module: '{module}' with query '{query}'")
+    resp = requests.post(BASE_URL + f"Categories/Filter?pageNumber=1&query={quote(query)}", json=[], headers=HEADERS)
+    if resp.status_code != 200:
+            print(f"Failed to get modules: {resp.status_code}")
+            print(resp.text)
+            return resp.status_code
+    resp = resp.json()
+
+    print(resp)
+
+    m = resp["Results"][0]
+    return {"name": m["Name"], "id": m["Identity"], "category": m["CategoryTypeIdentity"]}
+
+
+def get_ical_for_modules(modules):
+    print("Generating ics for modules: " + ", ".join(modules))
+    ot_modules = []
+
+    for m in modules:
+        ot_mod = module_search(m)
+        if not ot_mod:
+            print("Couldn't find module: " + m)
+            continue
+        ot_modules.append(ot_mod)
+
+    req_data = {"CategoryIdentities": [m["id"] for m in ot_modules], "ViewOptions": get_view_options()}
+    print("Fetching events for modules:")
+    print(req_data)
+    events = requests.post(BASE_URL + "categories/events/filter", json=req_data, headers=HEADERS)
+    if events.status_code != 200:
+        print(events.status_code)
+        print(events.text)
+        return events.text
+
+    print(events.text)
+
+    timetable = events.json()
+    ical = Calendar()
+    for module in timetable:
+        m = " ".join(module["Name"].split(" ")[1:])
+        for event in module["CategoryEvents"]:
+            desc = event["Name"]
+            if len(event["ExtraProperties"]) > 1:
+                desc += " happens on weeks " + event["ExtraProperties"][1]["Value"]
+            url = ""
+            if len(event["ExtraProperties"]) > 2:
+                url = event["ExtraProperties"][2]["Value"]
+            ev = Event(name=m + ": " + event["EventType"],
+                description=desc,
+                location=event["Location"],
+                begin=datetime.fromisoformat(event["StartDateTime"]),
+                end=datetime.fromisoformat(event["EndDateTime"]),
+                url=url)
+            ical.events.add(ev)
+
+    return ical
+
+
+
